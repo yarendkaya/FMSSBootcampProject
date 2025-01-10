397a60a8fd91236df64227bf3763f6231f54c296
yarendemirkaya <yarendmrky65@gmail.com>
2025-01-10 18:54:33 +0300

    Added Navigation Compose

--- presentation/home/build.gradle.kts
+++ presentation/home/build.gradle.kts
@@ -64,6 +64,7 @@ dependencies {
     implementation(libs.hilt.android)
     ksp(libs.hilt.compiler)
     ksp(libs.hilt.android.compiler)
+    implementation(libs.hilt.navigation.compose)
 
     implementation(libs.androidx.runtime)
 
